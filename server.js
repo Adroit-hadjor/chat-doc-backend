47b2f3bd64aee30e6bbf73f425f89005429db4d9
ofoe <ofoe@ofoes-MacBook-Pro.local>
2025-10-10 18:33:34 +0000

    cors error fixed

--- server.js
+++ server.js
@@ -36,7 +36,8 @@ const app = express();
 app.use(bodyParser.json());
 
 // Serve static frontend (public/index.html)
-app.use(cors({ origin: ["http://localhost:8082"], credentials: false }));
+//app.use(cors({ origin: ["http://localhost:8082"], credentials: false }));
+app.use(cors());
 app.use(express.static("public"));
 
 const upload = multer({ storage: multer.memoryStorage() });
